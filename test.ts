1c9ac2b39449d5bfc267b1e656ebaa9abc4e6002
anenasa <anenasaa@yahoo.com>
2026-08-08 09:40:26 +0800

    Up

--- test.ts
+++ test.ts
@@ -1,33 +1,26 @@
 import "./web/src/engine/RegExpSafe.ts";
 import "./web/src/engine/ArrayExtensions.ts";
 import * as websites from './web/src/engine/websites/_index';
-//import { CreateStorageController } from "./web/src/engine/StorageController.ts";
-//import { SettingsManager } from "./web/src/engine/SettingsManager.ts";
+import { CreateStorageController } from "./web/src/engine/StorageController.ts";
+import { SettingsManager } from "./web/src/engine/SettingsManager.ts";
 import { SetupCustomFetchProvider } from "./web/src/engine/platform/FetchProvider.ts";
 
 SetupCustomFetchProvider(undefined);
 
-//const storageController = CreateStorageController();
-//const settingsManager = new SettingsManager(storageController);
-//const provider = scraper.CreatePlugin(
-    //storageController,
-    //settingsManager
-//);
-
-const fakeProvider = {
-    Initialize() {},
-    CreateEntry(identifier: string, title: string) {
-        return { identifier, title };
-    }
-} as any;
+const storageController = CreateStorageController();
+const settingsManager = new SettingsManager(storageController);
 
 async function main() {
     const values = Object.values(websites);
     for (let index = 0; index < values.length; index++) {
         const website = values[index];
         const scraper = new website();
+        const provider = scraper.CreatePlugin(
+            storageController,
+            settingsManager
+        );
         console.log(index, scraper.Identifier);
-        const mangas = await scraper.FetchMangas(fakeProvider);
+        const mangas = await scraper.FetchMangas(provider);
         console.log(mangas[0].Identifier);
         const manga = mangas[0];
         //const manga = await scraper.FetchManga(
@@ -37,8 +30,7 @@ async function main() {
 
         const chapters = await scraper.FetchChapters(manga);
         const chapter = chapters[0];
-        await chapter.Update();
-        const pages = chapter.Entries.Value;
+        const pages = await scraper.FetchPages(chapter);
         const blob = await pages[0].Fetch(
             0,
             new AbortController().signal
