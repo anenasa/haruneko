fa548bf13cb13a5fcb09ecfb420b08767a485054
anenasa <anenasaa@yahoo.com>
2026-08-13 22:12:07 +0800

    extension: Remove options page

--- browser/popup.js
+++ browser/popup.js
@@ -1,11 +1,18 @@
-document.querySelector("#settings").addEventListener("click", () => {
-    browser.runtime.openOptionsPage();
-});
-document.querySelector("#haruneko").addEventListener("click", async () => {
-    const settings = await browser.storage.local.get({
+(() => {
+    const harunekoUrl = document.querySelector("#harunekoUrl");
+    browser.storage.local.get({
         harunekoUrl: "https://anenasa.github.io"
+    }).then((settings) => {
+        harunekoUrl.value = settings.harunekoUrl;
     });
-    await browser.tabs.create({
-        url: settings.harunekoUrl
+
+    document.querySelector("#open").addEventListener("click", () => {
+        browser.storage.local.set({
+            harunekoUrl: harunekoUrl.value
+        });
+        browser.tabs.create({
+            url: harunekoUrl.value
+        });
+        window.close();
     });
-});
+})();
