4286bfe989697d0f1db74d0e0fc4b385e2cbcccd
anenasa <anenasaa@yahoo.com>
2026-08-16 11:05:37 +0800

    Fix headers

--- firefox/background.js
+++ firefox/background.js
@@ -9,6 +9,36 @@
     });
 
     const hakunekoTabs = new Map();
+    const extensionUrl = browser.runtime.getURL("");
+
+    // Fix request headers
+    browser.webRequest.onBeforeSendHeaders.addListener((details) => {
+        if (details.documentUrl !== `${hakunekoUrl}/` && !details.documentUrl?.startsWith(extensionUrl))
+            return {};
+        if (details.url.startsWith(hakunekoUrl)) return {};
+        const fetchApiSupportedPrefix = 'X-FetchAPI-'.toLowerCase();
+        const oldHeaders = details.requestHeaders || [];
+        const newHeaders = [];
+        const removeSet = new Set();
+        for (const header of oldHeaders) {
+            // Avoid duplicated header entries
+            if (header.name.toLowerCase().startsWith(fetchApiSupportedPrefix)) {
+                removeSet.add(header.name.toLowerCase().substring(fetchApiSupportedPrefix.length));
+            }
+        }
+        for (const header of oldHeaders) {
+            if (header.name.toLowerCase().startsWith(fetchApiSupportedPrefix)) {
+                const newName = header.name.substring(fetchApiSupportedPrefix.length);
+                newHeaders.push({name: newName, value: header.value});
+            }
+            else {
+                if (!removeSet.has(header.name.toLowerCase())) {
+                    newHeaders.push(header);
+                }
+            }
+        }
+        return { requestHeaders: newHeaders };
+    }, { urls: ["<all_urls>"] },  ["blocking", "requestHeaders"]);
 
     // Enable CORS
     browser.webRequest.onHeadersReceived.addListener((details) => {
@@ -106,6 +136,10 @@
             executeScript(message.parameters).then(sendResponse);
             return true;
         }
+        if (message.channel === "FetchProvider.Fetch") {
+            fetchProviderFetch(message.parameters).then(sendResponse);
+            return true;
+        }
     });
 
     async function openTab(parameters, senderId) {
@@ -182,6 +216,33 @@
             });
             return {
                 result: result[0]
+            };
+        } catch (error) {
+            return {
+                error: error.message
+            };
+        }
+    }
+
+    // Calling fetch from extension
+    async function fetchProviderFetch(parameters) {
+        try {
+            const { url, method, headers, bodyUsed, body, credentials } = parameters[0];
+            const request = new Request(url, {
+                method,
+                headers: new Headers(headers),
+                body: bodyUsed ? body : undefined,
+                credentials
+            });
+            const response = await fetch(request);
+            return {
+                result: {
+                    ok: response.ok,
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: Object.fromEntries(response.headers),
+                    body: await response.arrayBuffer()
+                }
             };
         } catch (error) {
             return {
